c6dc6b5cd57d3bbd89dfd9435ea63e44d8ce4b6c
David Poisson <zeraaiur@hotmail.com>
2018-08-29 09:39:39 -0400

    created

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -14,6 +14,18 @@ class PricesController < ApplicationController
     end
   end
 
+  def edit
+    @fairepart_id = params[:id]
+    @fairepart = Fairepart.find_by(id: @fairepart_id)
+    @prices = Price.where(fairepart_id: @fairepart_id)
+  end
+
+  def update
+    params["prices"].each do |newprice|
+      Price.update(price_params(newprice))
+    end
+  end
+
   private
 
   def price_params(my_params)
