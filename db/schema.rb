7aff7344da4f57114c0b8fe51849c85351d2158d
David Poisson <zeraaiur@hotmail.com>
2019-02-07 10:31:13 -0500

    reponse first steps db

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,17 @@ ActiveRecord::Schema.define(version: 2018_09_12_163437) do
     t.index ["fairepart_id"], name: "index_prices_on_fairepart_id"
   end
 
+  create_table "reponses", force: :cascade do |t|
+    t.string "slug"
+    t.string "nom"
+    t.boolean "couleur"
+    t.string "format"
+    t.boolean "verso"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
